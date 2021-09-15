3950372b740045ef4c88848ffa362bfd617f6041
jweaston <jweaston@cic02.tacc.utexas.edu>
2021-09-15 16:37:29 -0500

    cleaned up elastic and monitoring

--- test_output.sh
+++ test_output.sh
@@ -1,1 +1,2 @@
-diff -r -B -q -w ~/tmp/tapis-deploy ~/from-mike/deployer-check/$1/tapis-deploy | grep '.yml' | grep -Ev '^Only in'
+diff -r -B -q -w ~/tapis-deploy ~/from-mike/tapis-deploy | grep '.yml' | grep 'Only in'
+
