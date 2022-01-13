9d17090517e70570de7586b19856546179947a9a
mpackard <mpackard>
2022-01-12 23:48:00 -0600

    first attempt at flattening config

--- tapis-api-generator.py
+++ tapis-api-generator.py
@@ -1,69 +1,13 @@
+import sys
+sys.path.append('lib')
+import deployer
+import os
 import argparse
 import yaml
-from jinja2 import Environment, FileSystemLoader, StrictUndefined
-import os
-from shutil import copy
-
-file_loader = FileSystemLoader('templates')
-env = Environment(loader=file_loader, undefined=StrictUndefined)
-env.trim_blocks = True
-env.lstrip_blocks = True
-env.rstrip_blocks = True
-
-def write_tapis(input_yaml, path, service_list, outDir = os.path.expanduser('~/tmp'), defaults_file="", uni_param="", ser_param={"data": ""}, is_sub=False):
-    for obj, data in input_yaml.items():
-        if obj == "universal-parameters":
-            uni_param = data 
-        elif obj == "service-parameters":
-            ser_param = data
-        elif "file_ext" in data:
-            file_path = path + obj + data["file_ext"]
-            if data["template"].split(".")[-1] == "j2":
-                if "template_directory" in ser_param['data']:
-                    new_path = path.split("/")
-                    new_path[0] = ser_param['data']["template_directory"]
-                    new_path = '/'.join(new_path) 
-                    template = env.get_template(new_path + data["template"])
-                    permissions = (os.stat("templates/" + new_path + data["template"])[0])
-                else:
-                    template = env.get_template(path + data["template"])
-                    permissions = (os.stat("templates/" + path + data["template"])[0])
-                def_param = defaults_file["defaults"]["universal"]
-                try:
-                    def_param.update(defaults_file["defaults"][ser_param["data"]["service_name"]])
-                except:
-                    pass
-                with open(outDir + "/tapis-deploy/" + file_path, "w") as file:
-                    file.write(template.render(local_param=data['data'], service_param=ser_param['data'], universal_param=uni_param['data'], service_list = service_list, defaults=def_param))
-                    os.chmod(outDir + "/tapis-deploy/" + file_path, permissions)
-        else:
-            if(obj not in service_list and not is_sub):
-                continue
-            try:
-                os.makedirs(outDir + "/tapis-deploy/" + path + obj)
-            except:
-                pass
-            write_tapis(data, path + obj + "/", service_list, outDir, defaults_file, uni_param, ser_param, True)   
-    return
-
-def create_tapis(template_path, service_list, outDir = os.path.expanduser('~/tmp')):
-    for root, _, files in os.walk(template_path, topdown=False):
-        service = root.split('/')[1]
-        if(service not in service_list and service != ""):
-            continue
-        root = root[root.index("/")+1:] + "/"
-        for name in files:
-            try:
-                os.makedirs(outDir + '/tapis-deploy/' + root)
-            except:
-                pass
-            if name.split(".")[-1] != "j2":
-                copy(template_path + root + name, outDir + "/tapis-deploy/" + root + name)
-
 
 def main():
 
-    import os 
+    project_name = 'tapis'
     basedir = os.path.dirname(os.path.realpath(__file__))
     print(basedir)
 
@@ -72,37 +16,45 @@ def main():
                         help="File location of yaml config")
 
     parser.add_argument('-o', dest='outDir', metavar='outDir', nargs='?',
-                        help="File location of program output, if not specified defaults to tmp/tapis-deploy")
+                        help="File location of program output, if not specified defaults to tmp/tapis")
 
     args = parser.parse_args()
     if not args.input:
         parser.print_help()
         exit(1)
 
-    parameters = yaml.safe_load(args.input)
-    f = open("defaults.yml", "r")
-    defaults = yaml.safe_load(f)
     outDir = args.outDir
-    service_list = parameters.get("services")
     file_path = ""
 
-    if(outDir):
-        outDir = outDir.strip()
-        try:
-            test = outDir+"/tapis-deploy/"
-            test2 = os.path.expanduser(test)
-            os.makedirs(test2)
-        except:
-            pass
-        create_tapis("templates/", service_list, os.path.abspath(outDir))
-        write_tapis(parameters, file_path, service_list, os.path.abspath(outDir), defaults_file=defaults)
-    else:
-        try:
-            os.makedirs(os.path.expanduser('~/tmp/tapis-deploy/'))
-        except:
-            pass
-        create_tapis("templates/", service_list)
-        write_tapis(parameters, file_path, service_list, defaults_file=defaults)
+    # import yaml input file. do not need "open" because type is set by argparse
+    input_data = yaml.safe_load(args.input)
+
+    # debug
+    print(input_data)
+    print(type(input_data))
+
+# todo: create python testing env
+# todo: test for duplicate vars
+# todo: test to ensure top level of input_data is type dict
+
+
+
+    # todo: reword? 
+    tapis_dir = '{}/{}'.format(outDir, project_name)
+    tapis_dir = os.path.expanduser(tapis_dir)
+    if not os.path.exists(tapis_dir):
+        os.makedirs(tapis_dir)
+    deployer.create_tapis("templates", tapis_dir, input_data)
+
+    #deployer.write_tapis(input_data, file_path, os.path.abspath(outDir))
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     main()
